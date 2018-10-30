f31e598e28a2a1043ece4939ee12fba4ca82f874
huong-nt <nguyentienhuong93@gmail.com>
2018-10-30 20:12:23 +0700

    install mqtt

--- tools/Install/pi.sh
+++ tools/Install/pi.sh
@@ -31,6 +31,8 @@ GSTREAMER_AUDIO_SINK="alsasink"
 
 install_dependencies() {
   sudo apt-get update
+  sudo apt-get -y install mosquitto mosquitto-clients libmosquittopp-dev
+  sudo systemctl enable mosquitto.service
   sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libcurl4-openssl-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip
   pip install flask commentjson
 }
